dd97b991f0a35da6fe0df1383824a46d5db780bb
leesh9 <lokciginer@gmail.com>
2024-06-19 10:23:42 +0900

    Fix: "Addr,User DAO/mapper 수정"

--- WangBam/src/main/java/mybatis/dao/AddrDAO.java
+++ WangBam/src/main/java/mybatis/dao/AddrDAO.java
@@ -1,5 +1,74 @@
 package mybatis.dao;
 
-public class AddrDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+
+import mybatis.service.FactoryService;
+import mybatis.vo.AddressVO;
+
+public class AddrDAO {
+    //주소추가
+    public int addAddr(Map<String, String> map) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        int cnt = ss.insert("addr.addAddr", map);
+        if(cnt > 0) {
+            ss.commit();
+        }
+        ss.close();
+        return cnt;
+    }
+
+    //주소가져오기
+    public AddressVO[] findAddrList(String us_idx) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        List<AddressVO> list = ss.selectList("addr.findAddrList", us_idx);
+        ss.close();
+        AddressVO[] arr = new AddressVO[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+    
+    //기본 주소 가져오기
+    public AddressVO findBasicAddr(String us_idx) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        AddressVO result = ss.selectOne("addr.findBasicAddr", us_idx);
+        ss.close();
+        return result;
+    }
+
+    //주소수정
+    public int updateAddr(Map<String, String> map) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        int cnt = ss.update("addr.updateAddr", map);
+        if(cnt > 0) {
+            ss.commit();
+        }
+        ss.close();
+        return cnt;
+    }
+    //기본주소 변경
+    public int updateDefaultAddr(Map<String, String> map) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        int cnt = ss.update("addr.updateDefaultAddr", map);
+        if(cnt > 0) {
+            ss.commit();
+        }
+        ss.close();
+        return cnt;
+    }
+
+    //주소삭제
+    public int deleteAddr(String ad_idx) {
+        SqlSession ss = FactoryService.getFactory().openSession();
+        int cnt = ss.delete("addr.deleteAddr", ad_idx);
+        if(cnt > 0) {
+            ss.commit();
+        }
+        ss.close();
+        return cnt;
+    }
 }
--- WangBam/src/main/java/mybatis/dao/UserDAO.java
+++ WangBam/src/main/java/mybatis/dao/UserDAO.java
@@ -1,24 +1,99 @@
 package mybatis.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import mybatis.service.FactoryService;
 import mybatis.vo.UserVO;
 
 public class UserDAO {
-	public int addUser(UserVO uvo) {
+	// 유저 추가
+	public int addUser(Map<String, String> map) {
 		SqlSession ss = FactoryService.getFactory().openSession();
-		int cnt = ss.insert("user.addUser",uvo);
+		int cnt = ss.insert("user.addUser",map);
+		if(cnt>0) {
+			ss.commit();
+		}
+		ss.close();
+		return cnt; 
+	}
+	
+	// 로그인
+	public UserVO login(Map<String, String> map) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		UserVO result = ss.selectOne("user.login",map);
+		ss.close();
+		return result;
+	}
+
+	//아이디 찾기
+	public String findId(Map<String, String> map) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		String result = ss.selectOne("user.findId",map);
+		ss.close();
+		return result;
+	}
+
+	// 비밀번호 찾기 
+	public String findPw(Map<String, String> map) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		String result = ss.selectOne("user.findPw",map);
+		ss.close();
+		return result;
+	}
+
+	// 유저 삭제
+	public int deleteUser(String us_idx) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		int cnt = ss.delete("user.deleteUser",us_idx);
 		if(cnt>0) {
 			ss.commit();
 		}
 		ss.close();
 		return cnt;
 	}
-	
-	public UserVO login(UserVO uvo) {
+
+	// 유저 수정
+	public int updateUser(Map<String, String> map) {
 		SqlSession ss = FactoryService.getFactory().openSession();
-		UserVO result = ss.selectOne("user.login",uvo);
+		int cnt = ss.update("user.updateUser",map);
+		if(cnt>0) {
+			ss.commit();
+		}
+		ss.close();
+		return cnt;
+	}
+
+	//모든유저
+	public UserVO[] AllUser() {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		List<UserVO> list = ss.selectList("user.AllUser");
+		UserVO[] arr = new UserVO[list.size()];
+		arr = list.toArray(arr);
+		ss.close();
+		return arr;
+	}	
+	//특정 유저 검색
+	public UserVO[] findUser(Map<String, String> map) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		List<UserVO> list = ss.selectList("user.findUser",map);
+		UserVO[] arr = new UserVO[list.size()];
+		if(list.size()>0&&list!=null) {
+			arr = list.toArray(arr);
+			ss.close();
+			return arr;
+		}
+		ss.close();
+		ss.close();
+		return arr;
+	}
+
+	//유저 한명 검색
+	public UserVO findUserInfo(String us_idx) {
+		SqlSession ss = FactoryService.getFactory().openSession();
+		UserVO result = ss.selectOne("user.findUserInfo",us_idx);
 		ss.close();
 		return result;
 	}
